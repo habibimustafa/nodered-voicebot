91bb4d6022e3027fd2d2b54d160e844922d79a60
Habibi Mustafa <bibitahabibi@gmail.com>
2017-03-08 22:03:27 +0700

    Revert "Change Worker Path recorder.js"
    This reverts commit 83c62081ad5bf7ee6046da63ea0e67bab8d25a8d.

--- js/recorderjs/recorder.js
+++ js/recorderjs/recorder.js
@@ -19,8 +19,7 @@ DEALINGS IN THE SOFTWARE.
 
 (function(window){
 
-  // var WORKER_PATH = 'js/recorderjs/recorderWorker.js';
-  var WORKER_PATH = 'https://rawgit.com/habibimustafa/nodered-voicebot/master/js/recorderjs/recorderWorker.js';
+  var WORKER_PATH = 'js/recorderjs/recorderWorker.js';
 
   var Recorder = function(source, cfg){
     var config = cfg || {};
